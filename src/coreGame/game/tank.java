ce5d3a2357aa18dbc65fe90fd8fa9d49845e2a48
kingofnonames <quocanhak57@gmail.com>
2024-05-30 18:47:13 +0700

    Xoa du thua

--- src/coreGame/game/tank.java
+++ src/coreGame/game/tank.java
@@ -115,48 +115,7 @@ public class tank implements collidible{
 		this.shootPressed = false;
 	}
 
-	public  boolean isIntersect(Rectangle rect1, Rectangle rect2) {
-       
-        if (rect1.x >= rect2.x + rect2.width || 
-            rect2.x >= rect1.x + rect1.width || 
-            rect1.y >= rect2.y + rect2.height || 
-            rect2.y >= rect1.y + rect1.height) {
-            return false;
-        }
-        return true;
-    }
-	public  boolean isIntersectRight(Rectangle rect1, Rectangle rect2) {
 
-        if (rect1.x +rect1.width < rect2.x + rect2.width && 
-            rect1.x +rect1.width > rect2.x ) {
-            return true;
-        }
-        return false;
-    }
-	public  boolean isIntersectLeft(Rectangle rect1, Rectangle rect2) {
-	       
-        if (rect1.x +rect1.width > rect2.x + rect2.width && 
-            rect1.x  < rect2.x + rect2.width ) {
-            return true;
-        }
-        return false;
-    }
-	public  boolean isIntersectBelow(Rectangle rect1, Rectangle rect2) {
-	    
-        if (rect1.y +rect1.height > rect2.y  && 
-            rect1.y  +rect1.height< rect2.y + rect2.height ) {
-            return true;
-        }
-        return false;
-    }
-	public  boolean isIntersectUp(Rectangle rect1, Rectangle rect2) {
-	    
-        if (rect1.y +rect1.height > rect2.y +rect2.height && 
-            rect1.y  < rect2.y + rect2.height ) {
-            return true;
-        }
-        return false;
-    }
 	
 	void update(tank other) {
 		
